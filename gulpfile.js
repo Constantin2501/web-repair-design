5f3d9bf2879743133bb946e5e0fca1ad2e7b335f
Konstantin <ckwebmast@gmail.com>
2024-08-20 15:50:21 +0400

    add header in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 function defaultTask(cb) {
 	// place code for your default task here
@@ -19,3 +21,11 @@ gulp.task('browser-sync', function() {
 	});
 	gulp.watch("src/*.html").on('change', browserSync.reload);
 });
+
+// gulp.task('mincss', async function () {
+// 	return gulp.src('src/**/*.css')
+// 		.pipe(cssmin())
+// 		.pipe(rename({suffix: '.min'
+// 		}))
+// 		.pipe(gulp.dest('.dist'));
+// });
