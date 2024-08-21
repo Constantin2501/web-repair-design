fb554b6ba91b9006fdfbeadb9c3c047a0be765db
Konstantin <ckwebmast@gmail.com>
2024-08-21 17:19:30 +0400

    feat: add gulp-sass in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,37 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
-
-function defaultTask(cb) {
-	// place code for your default task here
-	cb();
-}
-
-exports.default = defaultTask
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+	serveSass();
 	browserSync.init({
 		server: {
 			baseDir: "./src"
 		},
 		browser: "firefox"
 	});
-	gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+	watch("src/*.html").on('change', browserSync.reload);
+	watch("src/sass/**/*.sass", serveSass);
+	watch("src/js/*.js").on('change', browserSync.reload);
+}
 
-// gulp.task('mincss', async function () {
-// 	return gulp.src('src/**/*.css')
+function serveSass() {
+	return src("src/sass/*.sass")
+		.pipe(sass())
+		.pipe(dest("src/css"))
+		.pipe(browserSync.stream());
+}
+
+// function ms () {
+// 	return src('src/css/*.css')
 // 		.pipe(cssmin())
-// 		.pipe(rename({suffix: '.min'
-// 		}))
-// 		.pipe(gulp.dest('.dist'));
-// });
+// 		.pipe(rename({suffix: '.min'}))
+// 		.pipe(dest('dist'));
+// }
+
+exports.serve = bs;
+// exports.cssmin = ms;
